050a50cdea01a0b3a941e3493f579ecf4718b0a9
Abigail Demsas <ademsas1026@gmail.com>
2018-08-08 16:14:02 -0400

    some code

--- client/index.js
+++ client/index.js
@@ -6,6 +6,9 @@ import history from './history';
 import store from './store';
 import App from './app';
 
+// establishes socket connection
+import './socket'
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
